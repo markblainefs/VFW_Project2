f10bb8c46b92ab892c57bb454e3c206a09c141f5
Mark Blaine <markopollo@unknown68a86d4158f4.gateway.2wire.net>
2012-07-12 21:30:53 -0500

    Added getData

--- js/main.js
+++ js/main.js
@@ -57,6 +57,32 @@ window.addEventListener("DOMContentLoaded", function(){
 		alert("Honey Do Saved!");
 	}
 	
+	function getData(){
+		//Write data from local storage to the browser
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id", "items");
+		var makeList = document.createElement('ul');
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		for (var i=0, len=localStorage.length; i<len; i++){
+			var makeli = document.createElement('li');
+			makeList.appendChild(makeli);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			//Convert string from local storage value back to an object by using JSON.parse
+			var obj = JSON.parse(value);
+			var makeSubList = document.createElement('ul');
+			makeli.appendChild(makeSubList);
+			for (var n in obj){
+				var makeSubli = document.createElement('li');
+				makeSubList.appendChild(makeSubli);
+				var optSubText = obj[n][0]+" "+obj[n][1];
+				makeSubli.innerHTML = optSubText;
+				
+			}
+		}
+	}
+	
 	//Variable defaults
 	var locations = ["--Choose a Location--", "Home", "Car", "Errand"],
 		priorityValue;
@@ -65,12 +91,12 @@ window.addEventListener("DOMContentLoaded", function(){
 	makeList();
 	
 	//Set Link & Submit Click Events
-	/*
-var displayLink = $('displayLink');
+	var displayLink = $('displayLink');
 	displayLink.addEventListener("click", getData);
+/*
 	var clearLink = $('clear');
 	clearLink.addEventListener("click", clearLocal);
-*/	
+*/
 	var save = $('submit');
 	save.addEventListener("click", storeData);
 
